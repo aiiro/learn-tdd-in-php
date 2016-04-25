8c1146418e6244f68b870016f0fba25009ade681
aiiro <akairo29@gmail.com>
2016-04-25 23:50:10 +0900

    Add Jenkins ver. 1.658 recipe.

---
+++ site-cookbooks/jenkins/recipes/rpm.rb
@@ -0,0 +1,19 @@
+package "java-1.8.0-openjdk" do
+  action :install
+end
+
+remote_file "#{Chef::Config[:file_cache_path]}/jenkins-1.658-1.1.noarch.rpm" do
+  source "http://pkg.jenkins-ci.org/redhat/jenkins-1.658-1.1.noarch.rpm"
+  not_if "rpm -qa | grep -q '^jenkins'"
+  action :create
+  notifies :install, "rpm_package[jenkins]", :immediately
+end
+
+rpm_package "jenkins" do
+  source "#{Chef::Config[:file_cache_path]}/jenkins-1.658-1.1.noarch.rpm"
+  action :nothing
+end
+
+service "jenkins" do
+  action [:enable, :start]
+end
